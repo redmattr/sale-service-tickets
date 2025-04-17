3cda33f3847e0877e7f840a6c828824e310ee97c
lperetik <156618939+lperetik@users.noreply.github.com>
2025-04-16 21:02:06 -0400

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import sqlite3
+import os
 
 from flask import current_app, g
 import click 
 
-#Establishes a connection to the database
+# Establishes a connection to the database
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
@@ -15,20 +16,19 @@ def get_db():
 
     return g.db
 
-    
 # If there is a db connection current upon request, close connection
 def close_db(e=None):
     db = g.pop('db', None)
     if db is not None:
         db.close()
 
-#Initialize database with standard schema and default values
+# Initialize database with standard schema and default values
 def init_db():
     db = get_db()
 
-    with current_app.open_resource('events_ticketing_schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
+    schema_path = os.path.join(os.path.dirname(__file__), 'events_ticketing_schema.sql')
+    with open(schema_path, 'r', encoding='utf8') as f:
+        db.executescript(f.read())
 
 @click.command('init-db')
 def init_db_command():
