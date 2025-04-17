5a87bb0cf4fc95d86480b8f37d30898f1f32bd2b
Matthew Reddy <167944034+redmattr@users.noreply.github.com>
2025-04-16 20:39:17 -0400

    Fixed KeyError

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask, session
 from routes import routes
 
 app = Flask(__name__)
+app.config['DATABASE'] = '//database//sqlite://pyDB.db'
 app.secret_key = 'your_secret_key'  # Needed for session management
 
 # Imports and initializes database from the database folder
